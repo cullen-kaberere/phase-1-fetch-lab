9e3e053e05622ad75e3e6480139593a8b36b3fe4
cullen kaberere <kabesh86@gmail.com>
2024-10-19 12:16:07 +0300

    Update js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(booksData => renderBooks(booksData));
   
 }
 
@@ -11,6 +14,7 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
